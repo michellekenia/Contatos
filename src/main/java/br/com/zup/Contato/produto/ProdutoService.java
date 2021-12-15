72c019c87fb4bd044095830cc96479c25719e66e
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-15 18:01:50 -0300

    Criar metodo para cadastrar produtos

--- src/main/java/br/com/zup/Contato/produto/ProdutoService.java
+++ src/main/java/br/com/zup/Contato/produto/ProdutoService.java
@@ -1,4 +1,18 @@
 package br.com.zup.Contato.produto;
 
+import br.com.zup.Contato.contatos.dtos.ContatoDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    private List<ProdutoDTO> produtos = new ArrayList<>();
+
+    public void cadastrarProdutos(ProdutoDTO produto) {
+        produtos.add(produto);
+    }
+
 }
