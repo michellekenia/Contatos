5c358c59b798989519664b1ed92adbd710f506c4
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-15 16:59:23 -0300

    Implementar metodo de cadastrar contato

--- src/main/java/br/com/zup/Contato/contatos/ContatoController.java
+++ src/main/java/br/com/zup/Contato/contatos/ContatoController.java
@@ -1,4 +1,21 @@
 package br.com.zup.Contato.contatos;
 
+import br.com.zup.Contato.contatos.dtos.ContatoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contatos")
 public class ContatoController {
+
+    @Autowired
+    private ContatoService contatoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarcontato(@RequestBody ContatoDTO contato) {
+        contatoService.cadastrarContato(contato);
+    }
+
 }
