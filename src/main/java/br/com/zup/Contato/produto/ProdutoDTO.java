25b37de3b40ea065c9aee9bdb84ad168f0a922a6
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-15 15:05:00 -0300

    Criar DTO de produto

--- src/main/java/br/com/zup/Contato/produto/ProdutoDTO.java
+++ src/main/java/br/com/zup/Contato/produto/ProdutoDTO.java
@@ -1,4 +1,32 @@
 package br.com.zup.Contato.produto;
 
 public class ProdutoDTO {
+
+    private String nomeDoProduto;
+    private double valorDoProduto;
+
+    public ProdutoDTO() {
+    }
+
+    public ProdutoDTO(String nomeDoProduto, double valorDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+        this.valorDoProduto = valorDoProduto;
+    }
+
+    public String getNomeDoProduto() {
+        return nomeDoProduto;
+    }
+
+    public void setNomeDoProduto(String nomeDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+    }
+
+    public double getValorDoProduto() {
+        return valorDoProduto;
+    }
+
+    public void setValorDoProduto(double valorDoProduto) {
+        this.valorDoProduto = valorDoProduto;
+    }
+
 }
