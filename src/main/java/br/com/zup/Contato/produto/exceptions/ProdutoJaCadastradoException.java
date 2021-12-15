f83bf092bc1deb020e84a25addc43a3ba1ef4819
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-15 18:47:10 -0300

    Criar exception do produto

--- src/main/java/br/com/zup/Contato/produto/exceptions/ProdutoJaCadastradoException.java
+++ src/main/java/br/com/zup/Contato/produto/exceptions/ProdutoJaCadastradoException.java
@@ -1,4 +1,7 @@
-package br.com.zup.Contato.contatos.exceptions;
+package br.com.zup.Contato.produto.exceptions;
 
-public class ProdutoJaCadastradoException {
+public class ProdutoJaCadastradoException extends RuntimeException {
+    public ProdutoJaCadastradoException(String message) {
+        super(message);
+    }
 }
