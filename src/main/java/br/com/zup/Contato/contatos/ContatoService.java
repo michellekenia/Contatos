cd9b6a18c653a2a81817985ea8e52291d8e091a3
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-15 15:54:18 -0300

    Criar metodo para salvar contato

--- src/main/java/br/com/zup/Contato/contatos/ContatoService.java
+++ src/main/java/br/com/zup/Contato/contatos/ContatoService.java
@@ -1,4 +1,20 @@
 package br.com.zup.Contato.contatos;
 
+import br.com.zup.Contato.contatos.dtos.ContatoDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ContatoService {
+
+    private List<ContatoDTO> contatos = new ArrayList<>();
+
+    public void cadastrarContato(ContatoDTO contato) {
+
+        contatos.add(contato);
+
+    }
+
 }
