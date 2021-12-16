b3cb6f6fa1c0990e7a9a0bda47b3c99067ba9ce8
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-16 13:52:58 -0300

    Incluir validações nos atributos

--- src/main/java/br/com/zup/Contato/contatos/dtos/ContatoDTO.java
+++ src/main/java/br/com/zup/Contato/contatos/dtos/ContatoDTO.java
@@ -2,12 +2,20 @@ package br.com.zup.Contato.contatos.dtos;
 
 import br.com.zup.Contato.produto.dtos.ProdutoDTO;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class ContatoDTO {
 
+    @Email
     private String email;
+    @Size (min = 2 , max = 30, message = "O nome deve ter entre 2 e 30 caracteres")
+    @NotBlank
     private String nome;
+    @NotNull
     private String telefone;
     private List <ProdutoDTO> produtos;
 
