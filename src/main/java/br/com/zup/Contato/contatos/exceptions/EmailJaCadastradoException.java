2095b52bf5e1cb3fa62e678b1c9d9f10b800c5f7
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-16 14:57:23 -0300

    Criar excecao para cadastro repetido

---
+++ src/main/java/br/com/zup/Contato/contatos/exceptions/EmailJaCadastradoException.java
@@ -0,0 +1,8 @@
+package br.com.zup.Contato.contatos.exceptions;
+
+public class EmailJaCadastradoException extends RuntimeException {
+
+    public EmailJaCadastradoException(String message) {
+        super(message);
+    }
+}
