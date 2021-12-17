ce4b9015d080980502d6e3a1bf8c2a0ee010e440
Michelle Kênia <keniamsoaress@gmail.com>
2021-12-17 17:08:53 -0300

    Criar excecoes personalizadas na controller advisor

--- src/main/java/br/com/zup/Contato/config/ControladorAdvisor.java
+++ src/main/java/br/com/zup/Contato/config/ControladorAdvisor.java
@@ -1,4 +1,52 @@
 package br.com.zup.Contato.config;
 
+import br.com.zup.Contato.contatos.exceptions.ContatoNaoEncontradoException;
+import br.com.zup.Contato.contatos.exceptions.EmailJaCadastradoException;
+import br.com.zup.Contato.contatos.exceptions.ProdutoJaCadastradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControladorAdvisor {
+
+    @RestControllerAdvice
+    public class ControllerAdvisor {
+
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+
+        public List<MensagemDeErro> manipularErrosDeValidacao(MethodArgumentNotValidException exception) {
+            List<MensagemDeErro> erros = new ArrayList<>();
+            for (FieldError fieldError : exception.getFieldErrors()) {
+                MensagemDeErro erroValidacao = new MensagemDeErro(exception.getMessage(), fieldError.getDefaultMessage());
+                erros.add(erroValidacao);
+
+            }
+            return erros;
+        }
+
+        @ExceptionHandler(ContatoNaoEncontradoException.class)
+        @ResponseStatus(HttpStatus.NOT_FOUND)
+        public MensagemDeErro manipularExcecaoDeContatoNaoEncontrado(ContatoNaoEncontradoException exception) {
+            return new MensagemDeErro(exception.getMessage(), "Esse contato não existe.");
+        }
+
+        @ExceptionHandler(EmailJaCadastradoException.class)
+        @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+        public MensagemDeErro manipularExcecaoDeEmailJacadastrado(EmailJaCadastradoException exception) {
+            return new MensagemDeErro(exception.getMessage(), "Esse email já foi cadastrado.");
+        }
+
+        @ExceptionHandler(ProdutoJaCadastradoException.class)
+        @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+        public MensagemDeErro manipularExcecaoDeProdutoJacadastrado(ProdutoJaCadastradoException exception) {
+            return new MensagemDeErro(exception.getMessage(), "Esse produto já foi cadastrado.");
+        }
+    }
 }
